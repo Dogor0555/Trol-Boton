9f6859f46ada1c3ed274036f343ca51379d0c81c
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2020-11-18 20:08:48 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,21 @@
+function abrir(){
+            document.getElementById("ventana-emergente").style.display="block";
+        }
+
+            function cerrar(){
+                document.getElementById("ventana-emergente").style.display="none";
+            }
+
+function mueveElBoton(){
+            width = window.innerWidth;
+            height = window.innerHeight;
+
+            newWidth = (Math.random() * width);
+            newHeight = (Math.random() * height);
+
+            document.getElementById('btnNo').style.position = "absolute";
+            document.getElementById('btnNo').style.left = newWidth + "px";
+            document.getElementById('btnNo').style.top = newHeight + "px";
+            
+
+}
